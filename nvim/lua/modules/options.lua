84f0f628e373e12071c407f90a620c4bb97bf8c2
Rafael Breno <rafael_breno@outlook.com>
2021-07-14 10:01:18 -0300

    fix: Uncomment format options

--- nvim/lua/modules/options.lua
+++ nvim/lua/modules/options.lua
@@ -72,13 +72,13 @@ o.updatetime     = 1000 -- set faster update time
 o.joinspaces     = false
 
 -- stolen from tjdevries
---o.formatoptions = o.formatoptions
-  --- "a" -- Auto formatting is BAD.
-  --- "t" -- Don't auto format my code. I got linters for that.
-  --+ "c" -- In general, I like it when comments respect textwidth
-  --+ "q" -- Allow formatting comments w/ gq
-  --- "o" -- O and o, don't continue comments
-  --+ "r" -- But do continue when pressing enter.
-  --+ "n" -- Indent past the formatlistpat, not underneath it.
-  --+ "j" -- Auto-remove comments if possible.
-  --- "4" -- I'm not in gradeschool anymore
+o.formatoptions = o.formatoptions
+  - "a" -- Auto formatting is BAD.
+  - "t" -- Don't auto format my code. I got linters for that.
+  + "c" -- In general, I like it when comments respect textwidth
+  + "q" -- Allow formatting comments w/ gq
+  - "o" -- O and o, don't continue comments
+  + "r" -- But do continue when pressing enter.
+  + "n" -- Indent past the formatlistpat, not underneath it.
+  + "j" -- Auto-remove comments if possible.
+  - "4" -- I'm not in gradeschool anymore
