8f54cb547c7b0c027003a66b1dc16878adbff35d
Rafael <rafael_breno@outlook.com>
2022-08-23 20:39:52 -0300

    feat: Add tree-sitter

--- nvim/lua/tree-sitter.lua
+++ nvim/lua/tree-sitter.lua
@@ -1,13 +1,21 @@
 local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
 
-parser_config.haskell = {
-  install_info = {
-    url = "../../plugger/tree-sitter-haskell",
-    files = { "src/parser.c", "src/parser.cc" },
-  }
-}
-
 require'nvim-treesitter.configs'.setup {
+  ensure_installed = { 
+    'bash',
+    'dockerfile',
+    'gitignore',
+    'go', 
+    'gomod', 
+    'json',
+    'json5',
+    'lua',
+    'proto',
+    'rust',
+    'yaml',
+  },
+  sync_install = false,
+  auto_install = true,
   highlight = {
     enable = true
   },
