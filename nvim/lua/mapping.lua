2a2e6f2ad8006f48eb3814a4bb551ce1c1d33f50
Rafael Breno <rafael_breno@outlook.com>
2023-02-14 15:29:41 -0300

    feat: Add std-plugins lua file

--- nvim/lua/mapping.lua
+++ nvim/lua/mapping.lua
@@ -5,5 +5,5 @@ local g = vim.g;
 ---- with the same key because I don't want to 
 ---- miscreate a shortcut that only works for
 ---- specific files.
-g.mapleader = ','; -- Map Leader
+g.mapleader      = ','; -- Map Leader
 g.maplocalleader = ','; -- Map Local Leader
---
+++ nvim/lua/std-plugins.lua
@@ -0,0 +1,13 @@
+local g = vim.g;
+
+-- Disabling builtin plugins that I don't use
+g.loaded_gzip         = 1
+g.loaded_tar          = 1
+g.loaded_tarPlugin    = 1
+g.loaded_zipPlugin    = 1
+g.loaded_2html_plugin = 1
+g.loaded_netrw        = 1
+g.loaded_netrwPlugin  = 1
+g.loaded_matchit      = 1
+g.loaded_matchparen   = 1
+g.loaded_spec         = 1
