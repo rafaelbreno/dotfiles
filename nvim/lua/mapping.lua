6887cdab89fdb7b4df1cdb73e8e439edeeaad9e0
Rafael Breno <rafael_breno@outlook.com>
2023-02-14 17:08:16 -0300

    feat: Rewrite a few mappings in Lua

--- nvim/lua/mapping.lua
+++ nvim/lua/mapping.lua
@@ -1,9 +1,90 @@
 local g = vim.g;
 
--- Leader
----- Here I'm setting both Leader and Local Leader
----- with the same key because I don't want to 
----- miscreate a shortcut that only works for
----- specific files.
+---- Leader
+--  Here I'm setting both Leader and Local Leader
+-- with the same key because I don't want to 
+-- miscreate a shortcut that only works for
+-- specific files.
 g.mapleader      = ','; -- Map Leader
 g.maplocalleader = ','; -- Map Local Leader
+
+------ Key Mapping
+---- Declarations 
+-- Set Function
+local set = vim.keymap.set;
+
+-- silent option - <silent>
+local sOpt = { silent = true };
+-- No Remap option - <noremap>
+local nOpt = { silent = true };
+-- <silent> & <noremap>
+local snOpt = { silent = true, noremap = true };
+
+---- Plugins
+-- NvimTree
+set({'n'}, '<leader>ne', ':NvimTreeFocus<cr>', snOpt)
+set({'n'}, '<leader>nt', ':NvimTreeToggle<cr>', snOpt)
+set({'n'}, '<leader>nr', ':NvimTreeRefresh<cr>', snOpt)
+
+-- LazyGit
+set({'n'}, '<leader>lg', ':LazyGit<CR>', snOpt)
+
+-- Telescope
+set({'n'}, '<leader>ff', '<cmd>Telescope find_files<cr>', snOpt)
+set({'n'}, '<leader>fg', '<cmd>Telescope live_grep<cr>', snOpt)
+set({'n'}, '<leader>fb', '<cmd>Telescope buffers<cr>', snOpt)
+set({'n'}, '<leader>fh', '<cmd>Telescope help_tags<cr>', snOpt)
+
+-- Trouble
+set({'n'}, '<leader>xx', '<cmd>TroubleToggle<cr>', snOpt)
+set({'n'}, '<leader>xw', '<cmd>TroubleToggle lsp_workspace_diagnostics<cr>', snOpt)
+set({'n'}, '<leader>xd', '<cmd>TroubleToggle document_diagnostics<cr>', snOpt)
+set({'n'}, '<leader>xq', '<cmd>TroubleToggle quickfix<cr>', snOpt)
+set({'n'}, '<leader>xl', '<cmd>TroubleToggle loclist<cr>', snOpt)
+set({'n'}, 'gR', '<cmd>TroubleToggle lsp_references<cr>', snOpt)
+
+---- General
+-- Move between Terminals
+set({'t'}, '<C-h>', '<C-\\><C-n><C-w><C-h>', snOpt)
+set({'t'}, '<C-j>', '<C-\\><C-n><C-w><C-j>', snOpt)
+set({'t'}, '<C-k>', '<C-\\><C-n><C-w><C-k>', snOpt)
+set({'t'}, '<C-l>', '<C-\\><C-n><C-w><C-l>', snOpt)
+set({'n'}, '<C-h>', '<C-w><C-h>')
+set({'n'}, '<C-j>', '<C-w><C-j>')
+set({'n'}, '<C-k>', '<C-w><C-k>')
+set({'n'}, '<C-l>', '<C-w><C-l>')
+
+-- Resize Terminal
+set({'t'}, '<Left> ', '<C-><C-n>:vertical resize +2<CR>', sOpt)
+set({'t'}, '<Right>', '<C-><C-n>:vertical resize -2<CR>', sOpt)
+set({'t'}, '<Up>', '<C-><C-n>:resize   +2<CR>', sOpt)
+set({'t'}, '<Down>', '<C-><C-n>:resize   -2<CR>', sOpt)
+set({'n'}, '<Left>', '<CMD>vertical resize +2<CR>', sOpt)
+set({'n'}, '<Right>', '<CMD>vertical resize -2<CR>', sOpt)
+set({'n'}, '<Up>', '<CMD>resize   +2<CR>', sOpt)
+set({'n'}, '<Down>', '<CMD>resize   -2<CR>', sOpt)
+
+-- Save/Exit
+set({'n'}, '<leader>w', ':w<cr>', sOpt)
+set({'n'}, '<leader>q', ':wq<cr>', sOpt)
+
+-- remove annoying exmode
+set({'n'}, 'Q', '<Nop>', nOpt)
+set({'n'}, 'q:', '<Nop>', nOpt)
+
+-- Move Lines
+set({'n'}, '<A-j>', ':m .+1<CR>==', nOpt)
+set({'n'}, '<A-k>', ':m .-2<CR>==', nOpt)
+set({'i'}, '<A-j>', '<Esc>:m .+1<CR>==gi', nOpt)
+set({'i'}, '<A-k>', '<Esc>:m .-2<CR>==gi', nOpt)
+set({'v'}, '<A-j>', ':m \'>+1<CR>gv=gv', nOpt)
+set({'v'}, '<A-k>', ':m \'<-2<CR>gv=gv', nOpt)
+
+-- Indent
+set({'v'}, '<', '<gv', nOpt)
+set({'v'}, '>', '>gv', nOpt)
+
+
+--
+set({'n'}, ' ', ' ', sOpt)
+
--- nvim/lua/mapping.vim
+++ nvim/lua/mapping.vim
@@ -1,34 +1,5 @@
-"NvimTree
-nmap <silent><leader>ne :NvimTreeFocus<cr>
-nmap <silent><leader>nt :NvimTreeToggle<cr>
-nmap <silent><leader>nr :NvimTreeRefresh<cr>
-
-" in terminal
-" move between window
-tnoremap <C-h> <C-\><C-n><C-w><C-h>
-tnoremap <C-j> <C-\><C-n><C-w><C-j>
-tnoremap <C-k> <C-\><C-n><C-w><C-k>
-tnoremap <C-l> <C-\><C-n><C-w><C-l>
-
-" resize
-tnoremap <Left>  <C-\><C-n>:vertical resize +2<CR>
-tnoremap <Right> <C-\><C-n>:vertical resize -2<CR>
-tnoremap <Up>    <C-\><C-n>:resize   +2<CR>
-tnoremap <Down>  <C-\><C-n>:resize   -2<CR>
-" go to normal mode using double esc
+"go to normal mode using double esc
 tnoremap <Esc><Esc> <C-\\><C-n>
-
-" better movement between windows
-nnoremap <C-h> <C-w><C-h>
-nnoremap <C-j> <C-w><C-j>
-nnoremap <C-k> <C-w><C-k>
-nnoremap <C-l> <C-w><C-l>
-
-" resize buffer easier
-nnoremap <Left>  <CMD>vertical resize +2<CR>
-nnoremap <Right> <CMD>vertical resize -2<CR>
-nnoremap <Up>    <CMD>resize   +2<CR>
-nnoremap <Down>  <CMD>resize   -2<CR>
 
 " move vertically by visual line on wrapped lines
 nnoremap j gj
@@ -36,10 +7,6 @@ nnoremap k gk
 
 " better yank behaviour
 nnoremap Y y$
-
-" remove annoying exmode
-nnoremap Q  <Nop>
-nnoremap q: <Nop>
 
 " copy to system clipboard
 vnoremap <A-y> "+y
@@ -53,23 +20,6 @@ nmap <silent><leader>rh :let @/=""<cr>
 " Enter Visual mode
 nnoremap <leader>lv <S-v>
 nnoremap <leader>v <C-v>
-
-" Highlights code for multiple indents without reselecting
-" better indenting experience
-vnoremap < <gv
-vnoremap > >gv
-
-"Move Line Up/Down
-nnoremap <A-j> :m .+1<CR>==
-nnoremap <A-k> :m .-2<CR>==
-inoremap <A-j> <Esc>:m .+1<CR>==gi
-inoremap <A-k> <Esc>:m .-2<CR>==gi
-vnoremap <A-j> :m '>+1<CR>gv=gv
-vnoremap <A-k> :m '<-2<CR>gv=gv
-
-"Save/Exit
-nmap <silent><leader>w :w<cr>
-nmap <silent><leader>q :wq<cr>
 
 
 "GoFmt
@@ -95,24 +45,6 @@ function! s:check_back_space() abort
   let col = col('.') - 1
   return !col || getline('.')[col - 1]  =~# '\s'
 endfunction
-
-
-" Telescope
-nnoremap <leader>ff <cmd>Telescope find_files<cr>
-nnoremap <leader>fg <cmd>Telescope live_grep<cr>
-nnoremap <leader>fb <cmd>Telescope buffers<cr>
-nnoremap <leader>fh <cmd>Telescope help_tags<cr>
-
-" LazyGit
-noremap <silent> <leader>lg :LazyGit<CR>
-
-" Trouble
-nnoremap <leader>xx <cmd>TroubleToggle<cr>
-nnoremap <leader>xw <cmd>TroubleToggle lsp_workspace_diagnostics<cr>
-nnoremap <leader>xd <cmd>TroubleToggle document_diagnostics<cr>
-nnoremap <leader>xq <cmd>TroubleToggle quickfix<cr>
-nnoremap <leader>xl <cmd>TroubleToggle loclist<cr>
-nnoremap gR <cmd>TroubleToggle lsp_references<cr>
 
 " Toggle Folding
 nnoremap <leader>c za
