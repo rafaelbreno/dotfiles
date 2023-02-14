dcc08afa0f08a55a0460c35342d2df6db0507195
Rafael Breno <rafael_breno@outlook.com>
2023-02-14 15:28:14 -0300

    feat: Add Mapping lua

---
+++ nvim/lua/mapping.lua
@@ -0,0 +1,9 @@
+local g = vim.g;
+
+-- Leader
+---- Here I'm setting both Leader and Local Leader
+---- with the same key because I don't want to 
+---- miscreate a shortcut that only works for
+---- specific files.
+g.mapleader = ','; -- Map Leader
+g.maplocalleader = ','; -- Map Local Leader
