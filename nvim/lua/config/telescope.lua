fa346ec2561a2fd1974f0ea921966578c9aec318
Rafael Breno <rafael_breno@outlook.com>
2026-02-06 14:26:25 -0300

    Fix telescope

--- nvim/lua/config/telescope.lua
+++ nvim/lua/config/telescope.lua
@@ -1,10 +1,25 @@
 require("telescope").setup{
   defaults = {
+    vimgrep_arguments = {
+      "rg",
+      "--color=never",
+      "--no-heading",
+      "--with-filename",
+      "--line-number",
+      "--column",
+      "--smart-case",
+      "--hidden",
+    },
     file_ignore_patterns = {
       -- folders
       "vendor/", 
       -- files
       "%.lock"
+    }
+  },
+  pickers = {
+    find_files = {
+      hidden = true,
     }
   }
 }
